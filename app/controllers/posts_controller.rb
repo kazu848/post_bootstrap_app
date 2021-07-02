5b386855056f1595dcd846099069610d7d3e26a0
idakazuki <kazuki.fykgu@gmail.com>
2021-07-02 22:55:18 +0900

    新規投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,20 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to post
   end
 
+  private
+
+  def post_params
+    params.require(:post).permit(:titile, :content)
+  end
+  
   def edit
   end
 
